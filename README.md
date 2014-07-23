5bee522696b24e9042954e0b970b480271ed2221
Mark <elihusmails@gmail.com>
2014-07-22 21:37:49 -0400

    Shifted around some code.  There are less files and everything is in one place.

--- README.md
+++ README.md
@@ -2,3 +2,46 @@ myassistant
 ===========
 
 Project that brings together Python Flask and Python Celery into a system that will provide me with information I'm interested in
+
+Setting up the environment
+==========================
+
+"I really have no idea if this is all right or in the correct order.  This may be removed in the 
+future as I would imagine installing Flask and Celery are probably common"
+
+1. Execute the command: python virtualenv.py flask
+2. Execute the command: python setup.py
+
+
+Starting Things up
+==================
+
+1. Make sure RabbitMQ is running
+2. From the "app" folder, run the command: ../flask/bin/celery -A tasks.celery worker --loglevel=info
+3. From the top-level folder, run the command: python app/tasks.py
+4. In a browser, go to http://127.0.0.1:5000/combine.  You should get a response of "Sum is 6"
+
+Useful Links
+============
+http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html#installing-celery
+http://flask.pocoo.org/docs/patterns/celery/
+http://hairycode.org/2013/07/23/first-steps-with-celery-how-to-not-trip/
+https://github.com/thrisp/flask-celery-example
+
+
+Design
+======
+I want to keep this simple, as its a project that I hope grows and becomes useful.  I am learning 
+many things as I more forward with this project.  Things like Python, Flask, Celery, AMQP and RabbitMQ. 
+
+One interesting thing that I quickly realized while developing this code (I found some of it at the 
+flask-celery-example link above) is that since this is all asynchronous, the way in which this works is 
+to make 2 requests.  The first one submits the request and the second request get the results.  So to look 
+at the source code, The first call is made to http://127.0.0.1:5000/test.  This will submit the task to the 
+backend Celery system and get a task ID.  This task ID is returned to the user/browser/etc.  Then this data 
+is parsed and the task ID is sent back as a request using the URL http://127.0.0.1:5000/test/result/<task ID>.  
+So while there are 2 requests made, the work is being done while the first response is sent back, processed 
+and formed into the second request.  This is what makes the whole system asynchronous and provides a better 
+experience for the user.
+
+
--- app/tasks.py
+++ app/tasks.py
@@ -1,30 +1,125 @@
+from __future__ import absolute_import
+from os import path, environ
+import json
+from flask import Flask, Blueprint, abort, jsonify, request, session
+import settings
 from celery import Celery
 import urllib2
-import json
 
 
-app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
+app = Flask(__name__)
+app.config.from_object(settings)
 
-@app.task(name='tasks.doAdd')
+'''
+==========================================
+============= CELERY Section =============
+==========================================
+'''
+def make_celery(app):
+    celery = Celery(app.import_name, backend='amqp', broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+@celery.task(name='tasks.doAdd')
 def doAdd(x, y):
     return x + y
 
-@app.task(name='tasks.currentHomeTemp')
+@celery.task(name='tasks.currentHomeTemp')
 def currentHomeTemp():
-	f = urllib2.urlopen('http://api.wunderground.com/api/<INSERT-KEY>/geolookup/conditions/q/NY/Whitesboro.json')
-	json_string = f.read()
-	parsed_json = json.loads(json_string)
-	location = parsed_json['location']['city']
-	temp_f = parsed_json['current_observation']['temp_f']
-	f.close()
-	return "Current temperature in %s is: %s" % (location, temp_f)
+    f = urllib2.urlopen('http://api.wunderground.com/api/' 
+                        + app.config['WUNDERGROUND_KEY'] 
+                        + '/geolookup/conditions/q/NY/Whitesboro.json')
+    
+    json_string = f.read()
+    parsed_json = json.loads(json_string)
+    location = parsed_json['location']['city']
+    temp_f = parsed_json['current_observation']['temp_f']
+    f.close()
+    return "Current temperature in %s is: %s" % (location, temp_f)
 	
-@app.task(name='tasks.currentZipcodeTemp')
+@celery.task(name='tasks.currentZipcodeTemp')
 def currentZipcodeTemp(zipcode):
-	f = urllib2.urlopen('http://api.wunderground.com/api/<INSERT-KEY>/geolookup/conditions/q/' + zipcode + '.json')
+	f = urllib2.urlopen('http://api.wunderground.com/api/' 
+                        + app.config['WUNDERGROUND_KEY'] 
+                        + '/geolookup/conditions/q/' + zipcode + '.json')
+    
 	json_string = f.read()
 	parsed_json = json.loads(json_string)
 	location = parsed_json['location']['city']
 	temp_f = parsed_json['current_observation']['temp_f']
 	f.close()
 	return "Current temperature at zipcode %s is: %s" % (zipcode, temp_f)
+
+@celery.task(name="tasks.add")
+def add(x, y):
+    return x + y
+
+
+
+'''
+==========================================
+============= FLASK Section ==============
+==========================================
+'''
+@app.route("/test")
+def hello_world(x=16, y=16):
+    x = int(request.args.get("x", x))
+    y = int(request.args.get("y", y))
+    res = add.apply_async((x, y))
+    context = {"id": res.task_id, "x": x, "y": y}
+    result = "add((x){}, (y){})".format(context['x'], context['y'])
+    goto = "{}".format(context['id'])
+    return jsonify(result=result, goto=goto)
+
+@app.route("/test/result/<task_id>")
+def show_result(task_id):
+    retval = add.AsyncResult(task_id).get(timeout=1.0)
+    return repr(retval)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Hello World!!'
+
+@app.route('/combine')
+def combine():
+    val = doAdd.delay(2,4)
+    if val.ready():
+        return "Sum is " + str(val.get(timeout=1))
+    else:
+        return "Addition operation timed out"
+
+@app.route('/weather/home/temp/current')
+def homeWeather():
+    val = currentHomeTemp.delay()
+    if val.ready():
+        return str(val.get(timeout=5))
+    else:
+        return "Current temperature operation timed out"
+        
+@app.route('/weather/<zipcode>/temp/current')
+def currentTempAtZip(zipcode):
+    val = currentZipcodeTemp.delay(zipcode)
+    if val.ready():
+        return str(val.get(timeout=5, no_ack=False))
+    else:
+        return "Current zipcode temperature operation timed out"
+    
+    
+'''
+==========================================
+============== MAIN Section ==============
+==========================================
+'''
+if __name__ == "__main__":
+    port = int(environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
